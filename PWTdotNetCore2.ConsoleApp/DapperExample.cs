9b6b542588c91e4b9b290bab3c4298e0cece823e
OutKast <phonewinttun173@gmail.com>
2024-10-26 15:41:48 +0630

    feat: errors fixed

---
+++ PWTdotNetCore2.ConsoleApp/DapperExample.cs
@@ -0,0 +1,104 @@
+﻿using Dapper;
+using PWTdotNetCore2.ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWTdotNetCore2.ConsoleApp
+{
+    public class DapperExample
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=PWTDB;User ID=sa;Password=georgeedwards1735";
+
+        public void Read()
+        {
+            //using (IDbConnection db = new SqlConnection(_connectionString))
+            //{
+            //    string query = "SELECT * FROM tbl_blog WHERE DeleteFlag = 0 OR DeleteFlag IS NULL";
+            //    var lst = db.Query(query).ToList();
+            //    foreach (var item in lst)
+            //    {
+            //        Console.WriteLine(item.BlogId);
+            //        Console.WriteLine(item.BlogTitle);
+            //        Console.WriteLine(item.BlogAuthor);
+            //        Console.WriteLine(item.BlogContent);
+            //    }
+
+
+
+            //}
+
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT * FROM tbl_blog WHERE DeleteFlag = 0 OR DeleteFlag IS NULL";
+                var lst = db.Query<BlogDataModel>(query).ToList();
+                foreach (var item in lst)
+                {
+                    Console.WriteLine(item.BlogId);
+                    Console.WriteLine(item.BlogTitle);
+                    Console.WriteLine(item.BlogAuthor);
+                    Console.WriteLine(item.BlogContent);
+                }
+
+            }
+            // DTO = Data transfer object or models
+
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+     VALUES
+           (@BlogTitle
+		   ,@BlogAuthor
+           ,@BlogContent
+           ,0)";
+
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                int result = db.Execute(query, new BlogDataModel
+                {
+                    BlogTitle = title,
+                    BlogAuthor = author,
+                    BlogContent = content,
+                } ) ;
+
+                Console.WriteLine(result == 1 ? "Successfully saved." : "Save Failed.");
+            }
+
+        }
+
+        public void Edit(int id)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT * FROM tbl_blog WHERE (DeleteFlag = 0 OR DeleteFlag IS NULL) and BlogId = @BlogId;";
+                var item = db.Query<BlogDataModel>(query, new BlogDataModel
+                {
+                    BlogId = id
+                }).FirstOrDefault();
+
+
+                if (item is null) {
+                    Console.WriteLine("No data found.");                
+                    return;
+                }
+                {
+                    Console.WriteLine(item.BlogId);
+                    Console.WriteLine(item.BlogTitle);
+                    Console.WriteLine(item.BlogAuthor);
+                    Console.WriteLine(item.BlogContent);
+                }
+
+            }
+        }
+    }
+}
--- PWTdotNetCore2.ConsoleApp/Program.cs
+++ PWTdotNetCore2.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PWTdotNetCore.ConsoleApp;
+using PWTdotNetCore2.ConsoleApp;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
@@ -7,11 +8,16 @@ using System.Data.SqlClient;
 
  
 
-AdoDotNetExample2 adoDotNetExample = new AdoDotNetExample2();
+//AdoDotNetExample2 adoDotNetExample = new AdoDotNetExample2();
 //adoDotNetExample.Read();
 //adoDotNetExample.Create();
 //adoDotNetExample.Edit();
 //adoDotNetExample.Update();
 //adoDotNetExample.Delete();
 
+DapperExample dapperExample = new DapperExample();
+//dapperExample.Read();
+//dapperExample.Create("Roots", "Aldous Huxley", "Fiction");
+//dapperExample.Edit(1);
+//dapperExample.Edit(2);
 Console.ReadKey();
