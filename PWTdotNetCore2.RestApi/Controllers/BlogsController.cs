0708f4c66fe2917eb41da8cffcc047edba97148e
OutKast317 <phonewinttun173@gmail.com>
2024-12-25 21:28:57 +0630

    feat: create restAPI

---
+++ PWTdotNetCore2.RestApi/Controllers/BlogsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PWTdotNetCore2.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        [HttpGet]
+            public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+            public IActionResult Edit()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+            public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
